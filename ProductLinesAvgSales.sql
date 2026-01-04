5404ac86f5ad2c67158f83fdf8c49ddef848cd54
floria02 <158179707+floria02@users.noreply.github.com>
2026-01-04 23:37:50 +0200

    Delete ProductLinesAvgSales.sql

--- ProductLinesAvgSales.sql
+++
@@ -1,9 +0,0 @@
--- Which product lines have the highest average sales per order?
-select productline,round(avg(sales),2) as AvarageSalesOrder
-from salesdata
-group by productline
-order by avg(sales) desc;
-
-
-
-
