7715f72a2c8cf56a99b7e0b94131a96b9c1f4345
floria02 <158179707+floria02@users.noreply.github.com>
2026-01-11 18:29:21 +0200

    Add files via upload

---
+++ SQL_Regular_Ord.sql
@@ -0,0 +1,65 @@
+-- What product people aged 34,female buy the most? 
+select oo.age,oo.gender,o.product,sum(o.quantity) as quantity
+from orders1 o
+left join orders2 oo
+on o.customerid = oo.CustomerID
+where age = 34 and gender = 'F'
+group by oo.age,o.product
+order by quantity desc;
+
+
+-- What ages generated a revenue more than 1000 for the company?
+select oo.age,round(sum(o.netrevenue),2) as revenue
+from orders1 o
+inner join orders2 oo
+on o.customerid = oo.CustomerID
+group by oo.age
+having sum(o.netrevenue) > 4000
+order by revenue desc;
+
+
+-- In which country people tend to pay a higher average price?
+select oo.country,round(avg(o.totalprice),2) as avgprice
+from orders1 o
+left join orders2 oo
+on o.customerid = oo.CustomerID
+group by oo.country
+order by round(avg(o.totalprice),2) desc;
+
+-- Find products that start with C or P where the corresponding age is between 20 and 40. 
+select o.product,oo.age
+from orders1 o
+inner join orders2 oo
+on o.customerid = oo.CustomerID
+where (o.product like 'C%' or o.product like 'P%')
+and oo.age between 20 and 40 ;
+
+
+
+-- Show the average price in each row of the "referral source" column. 
+SELECT ReferralSource,TotalPrice,ROUND(AVG(TotalPrice) OVER (PARTITION BY ReferralSource), 2) AS AvgPrice
+FROM orders1;
+
+
+-- Show the average qantity in each row of the "products" column.
+select product,quantity,round(avg(Quantity) over (partition by Product),1) as AvgQuantity
+from orders1;
+
+
+-- Calculate the average price  in each row of the "payment method" column.
+SELECT paymentmethod,round(avg(totalprice) over (partition by paymentmethod),2) as avgprice 
+from orders1;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
